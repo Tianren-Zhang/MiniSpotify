446d29b0121fe34e5984396b8d4f268b25474711
ztrrrrr <84163642+ztrrrrr@users.noreply.github.com>
2023-09-13 14:43:39 -0400

    bottom nav

--- app/src/main/java/com/laioffer/spotify/MainActivity.kt
+++ app/src/main/java/com/laioffer/spotify/MainActivity.kt
@@ -1,39 +1,97 @@
 package com.laioffer.spotify
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
 import com.laioffer.spotify.ui.theme.SpotifyTheme
+
 
 // customized extend AppCompatActivity
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            SpotifyTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
-        }
+//        setContent {
+//            SpotifyTheme {
+//                // A surface container using the 'background' color from the theme
+//                Surface(
+//                    modifier = Modifier.fillMaxSize(),
+//                    color = MaterialTheme.colors.background
+//                ) {
+//                    AlbumCover()
+//                }
+//            }
+//        }
+
+        setContentView(R.layout.activity_main)
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun LoadSection(text: String) {
+    Row(
+        modifier = Modifier.padding(vertical = 8.dp)
+    ) {
+        Text(
+            text = text,
+            style = MaterialTheme.typography.body2,
+            color = Color.White
+        )
+    }
+}
+
+@Composable
+fun AlbumCover() {
+    Column {
+        Box(modifier = Modifier.size(160.dp)) {
+            AsyncImage(
+                modifier = Modifier.fillMaxSize(),
+                contentScale = ContentScale.FillBounds,
+                model = "https://upload.wikimedia.org/wikipedia/en/d/d1/Stillfantasy.jpg",
+                contentDescription = null
+            )
+
+            Text(
+                text = "Still Fantasy",
+                color = Color.White,
+                modifier = Modifier.padding(bottom = 4.dp, start = 4.dp).align(Alignment.BottomStart)
+            )
+        }
+
+        Text(
+            text = "Jay Chou",
+            modifier = Modifier.padding(start = 4.dp, top = 4.dp),
+            style = MaterialTheme.typography.body2.copy(fontWeight = FontWeight.Bold),
+            color = Color.LightGray,
+        )
+    }
+}
+
+@Preview
+@Composable
+fun PreviewAlbumCover() {
+    SpotifyTheme {
+        Surface {
+            AlbumCover()
+        }
+    }
 }
 
 @Preview(showBackground = true)
@@ -41,7 +99,7 @@ fun Greeting(name: String) {
 fun DefaultPreview() {
     SpotifyTheme {
         Surface {
-            Greeting("Android")
+            LoadSection("Screen is loading")
         }
     }
 }
